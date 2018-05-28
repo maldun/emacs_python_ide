33c89cfda26463b853c2966b40bb2c771d1c2fdf
Reiterer, Stefan <stefan.reiterer@v2c2.at>
2018-05-28 11:03:16 +0200

    added melpa in a way which works with different emacs versions

--- IDE.el
+++ IDE.el
@@ -2,12 +2,17 @@
 ;; --------------------------------------
 
 ;; Enable package stuff
+;; load melpa for package installing
 (require 'package)
-(package-initialize)
-
-(add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
-
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (proto (if no-ssl "http" "https")))
+  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
+  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
+  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
+  (when (< emacs-major-version 24)
+    ;; For important compatibility libraries like cl-lib
+    (add-to-list 'package-archives '("gnu" . (concat proto "://elpa.gnu.org/packages/")))))
 (package-initialize)
 (when (not package-archive-contents)
   (package-refresh-contents))
